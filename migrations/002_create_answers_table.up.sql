042b8673d17709334f3940cd41208bb7a5555c53
Roman Kolesnikov <roman.codev@gmail.com>
2025-05-15 13:11:21 +0300

    feat: init migrations

---
+++ migrations/002_create_answers_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS answers (
+    id SERIAL PRIMARY KEY,
+    user_tg_id BIGINT NOT NULL REFERENCES users (tg_id) ON DELETE CASCADE,
+    question_key TEXT NOT NULL,
+    step INT NOT NULL,
+    answer TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
---
+++ migrations/003_create_email_requests_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS email_requests (
+    id SERIAL PRIMARY KEY,
+    user_tg_id BIGINT NOT NULL REFERENCES users (tg_id) ON DELETE CASCADE,
+    body TEXT NOT NULL,
+    status TEXT DEFAULT 'pending' CHECK (
+        status IN ('pending', 'sent', 'failed')
+    ),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    sent_at TIMESTAMP
+);
